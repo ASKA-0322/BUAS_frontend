2d6072fc5a768ef9251eb33f0e4a97824b380ae4
wwwynwyn <731499592@qq.com>
2022-07-11 22:44:44 +0800

    风险年龄分析获取后端数据已完成

--- BUAS_frontend/src/views/riskAnalysis/ageRiskAnalysis.vue
+++ BUAS_frontend/src/views/riskAnalysis/ageRiskAnalysis.vue
@@ -7,7 +7,8 @@
 </template>
 
  <script>
-
+      //引入axios
+      import axios from 'axios';
      // 引入 echarts 核心模块，核心模块提供了 echarts 使用必须要的接口。
      import * as echarts from 'echarts/core';
      // 引入提示框，标题，直角坐标系，数据集，内置数据转换器组件，组件后缀都为 Component
@@ -28,12 +29,10 @@
          // 注册必须的组件
          echarts.use([
            TitleComponent,
-           PieChart,
            TooltipComponent,
            GridComponent,
            DatasetComponent,
            TransformComponent,
-           BarChart,
            LabelLayout,
            UniversalTransition,
            CanvasRenderer,
@@ -41,16 +40,40 @@
 
        },
   mounted() {
+
+    axios.get('http://172.20.10.4:8081/risk/age').then(function(response){//获取后端 用户--风险指数
+            var value =response.data
+            get_riskage(value)
+        })
+
+    function get_riskage(value){
+
+      axios.get('http://172.20.10.4:8081/risk/generation').then(function(response){//获取后端 用户--风险指数
+            var value1 =response.data
+            get_riskgeneration(value1)
+        })
+
+        function get_riskgeneration(value1){
+
     // 初始化图表，设置配置项
     var myChart = echarts.init(document.getElementById('main'));
-    const dataBJ = [
-      [18, 98],
-      [30, 34],
-      [22, 49],
-      [24, 46],
-      [65, 73],
-      [44, 30],
-    ];//年龄风险程度数据
+
+    //设置数据
+     var datarisk=[];
+     for (let i = 0; i < value.data.length; i++) {
+         let obj=[value.data[i].age,value.data[i].indexMark]
+         datarisk.push(obj)    //每次循环获取一个下标的记录，存在obj里面，再追加到data里面
+     }
+
+    const dataBJ =datarisk;
+    // [
+    //   [18, 0.0005],
+    //   [30, 0.0103],
+    //   [22, 0.0025],
+    //   [24, 0.0037],
+    //   [65, 0.0015],
+    //   [44, 0.0077],
+    // ];//年龄风险程度数据
 
 const schema = [
   { name: 'age', index: 0, text: '年龄' },
@@ -108,12 +131,12 @@ const itemStylehigh = {
        dataset: {
           source: [
             ['sex', '低风险', '中风险', '高风险'],
-            ['18~30', 3215, 8518, 9317],
-            ['30~40', 8351, 7314, 5511],
-            ['40~50', 8624, 6512, 8215],
-            ['50~60', 7214, 5319, 3911],
-            ['60~70', 8614, 6512, 8215],
-            ['70~80', 7214, 5319, 3911],
+            ['18~30', value1.data[0].low, value1.data[0].mid, value1.data[0].high],
+            ['30~40', value1.data[1].low, value1.data[1].mid, value1.data[1].high],
+            ['40~50', value1.data[2].low, value1.data[2].mid, value1.data[2].high],
+            ['50~60', value1.data[3].low, value1.data[3].mid, value1.data[3].high],
+            ['60~70', value1.data[4].low, value1.data[4].mid, value1.data[4].high],
+            ['70~80', value1.data[5].low, value1.data[5].mid, value1.data[5].high],
           ]//各年龄段三个风险等级数据
         },
        grid: [{
@@ -137,7 +160,7 @@ const itemStylehigh = {
            fontSize: 13
          },
          gridIndex: 0,
-         min:10,
+         min:18,
          max: 80,
          splitLine: {
            show: false
@@ -161,8 +184,8 @@ const itemStylehigh = {
          nameTextStyle: {
            fontSize: 16
          },
-         min:10,
-         max:100,
+         min:0.002,
+         max:0.02,
          gridIndex:0,
          splitLine: {
            show: false
@@ -185,8 +208,9 @@ const itemStylehigh = {
            left: '42%',
            top:'12.5%',
            dimension: 1,
-           min: 10,
-           max: 100,
+           min: 0.002,
+           max: 0.02,
+           precision:4,
            itemHeight: 380,
            seriesIndex: 0,
            text: ['明暗：风险程度'],
@@ -240,6 +264,8 @@ const itemStylehigh = {
        ]
      }
      myChart.setOption(option);    //调用工具
+        }
+     }
    },
    name: '',
  }
