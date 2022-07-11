2c23d328525cf43ad721a038a2c62561b0abeeb7
wwwynwyn <731499592@qq.com>
2022-07-11 19:49:31 +0800

    王以宁：用户消费预测获取后端数据已完成

--- BUAS_frontend/src/views/consumeAnalysis/userConsumeForecast.vue
+++ BUAS_frontend/src/views/consumeAnalysis/userConsumeForecast.vue
@@ -7,7 +7,8 @@
 </template>
 
  <script>
-
+    //引入axios
+    import axios from 'axios';
     // 引入 echarts 核心模块，核心模块提供了 echarts 使用必须要的接口。
     import * as echarts from 'echarts/core';
     // 引入饼图图表，图表后缀都为 Chart
@@ -35,7 +36,6 @@
           GridComponent,
           DatasetComponent,
           TransformComponent,
-          BarChart,
           LabelLayout,
           UniversalTransition,
           CanvasRenderer,
@@ -43,81 +43,117 @@
 
       },
       mounted () {
+
+        axios.get('http://172.20.10.4:8081/area/predict/consum').then(function(response){
+            var value =response.data
+            console.log(value)
+            get_predictconsum(value)
+        })
+        function get_predictconsum(value){
+
         // 初始化图表，设置配置项
         var myChart = echarts.init(document.getElementById('main'));
+
+        var data=[];
+        var yeardata=[];
+       // { value: value.data[1].amount, name: value.data[1].period },
+        for (let i = 0; i < value.data.length; i++) {
+            let obj={value:value.data[i].amount,name:value.data[i].payTime}
+            let yearobj=value.data[i].payTime
+            data.push(obj)    //每次循环获取一个下标的记录，存在obj里面，再追加到data里面
+            yeardata.push(yearobj)
+        }
+
         let option = {
   title: {
-    text: '消费金额'
+    text: '用户消费预测',
+    left:'center'
   },
   tooltip: {
     trigger: 'axis',
     axisPointer: {
-      type: 'shadow'
+      type: 'cross'
     }
   },
   legend: {
     show:true,
     orient: "",
     padding: [0, 10, 0, 20],
-    data:["2023年3月",
-            "2023年6月",
-            "2023年9月",
-            "2023年12月",
-            "2024年3月",
-            "2024年6月",
-            "2024年9月",
-            "2024年12月",
-            "2025年3月",
-            "2025年6月",
-            "2025年9月",
-            "2025年12月"]
+    data:yeardata
+    // ["2016",
+    //  "2017",
+    //  "2018",
+    //  "2019",
+    //  "2020",
+    //  "2021",
+    //  "2022",
+    //  "2023",
+    //  "2024"]
   },
   grid: {
-    left: '3%',
-    right: '4%',
+    left: '10%',
+    right: '10%',
     bottom: '3%',
     containLabel: true
   },
   xAxis: {
     type: 'category',
-    name:'class',
-    data: ["2023年3月",
-            "2023年6月",
-            "2023年9月",
-            "2023年12月",
-            "2024年3月",
-            "2024年6月",
-            "2024年9月",
-            "2024年12月",
-            "2025年3月",
-            "2025年6月",
-            "2025年9月",
-            "2025年12月"]
+    name:'年份',
+    data:yeardata
+    // ["2016",
+    //  "2017",
+    //  "2018",
+    //  "2019",
+    //  "2020",
+    //  "2021",
+    //  "2022",
+    //  "2023",
+    //  "2024"]
   },
   yAxis: {
     type: 'value',
+    name:'消费金额',
+    scale:true,
     boundaryGap: [0, 0.01]
   },
   series: [
     {
-      name:'test',
+      name:'消费金额',
       type: 'line',
-      data: [{ value: 17761, name: "2023年3月" },
-              { value: 18203, name: "2023年6月" },
-              { value: 26971, name: "2023年9月" },
-              { value: 25484, name: "2023年12月" },
-              { value: 37546, name: "2024年3月" },
-              { value: 35677, name: "2024年6月" },
-              { value: 51973, name: "2024年9月" },
-              { value: 49949, name: "2024年12月" },
-              { value: 72943, name: "2025年3月" },
-              { value: 69928, name: "2025年6月" },
-              { value: 94479, name: "2025年9月" },
-              { value: 97900, name: "2025年12月" },]
+      data:data,
+              // [{ value: 17761, name: "2023年3月" },
+              // { value: 18203, name: "2023年6月" },
+              // { value: 26971, name: "2023年9月" },
+              // { value: 25484, name: "2023年12月" },
+              // { value: 37546, name: "2024年3月" },
+              // { value: 35677, name: "2024年6月" },
+              // { value: 51973, name: "2024年9月" },
+              // { value: 49949, name: "2024年12月" },
+              // { value: 72943, name: "2025年3月" },
+              // { value: 69928, name: "2025年6月" },
+              // { value: 94479, name: "2025年9月" },
+              // { value: 97900, name: "2025年12月" },]
+      markArea: {
+        itemStyle: {
+          color: '#90d7ec'
+        },
+        data: [
+          [
+            {
+              name: '预测金额',
+              xAxis: '2022'
+            },
+            {
+              xAxis: '2024'
+            }
+          ]
+        ]
+      }
     }
   ]
 };
         myChart.setOption(option);    //调用工具
+        }
       },
       name: '',
     }
